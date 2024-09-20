9ad4d00fb3019f6cf97de36e5bd5b653cd26eb6f
GuylordSS <sagun.guylordsebastian56@gmail.com>
2024-09-20 17:28:43 +0800

    Loginpage-ADD-logo

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -23,9 +23,11 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+    <div className="flex items-center justify-center min-h-screen bg-vlu">
+      <div className="w-full max-w-md p-8 ">
+      <div class="mb-3 flex justify-center">
+      <img src="/images/logo1.png" alt="DocQuestLogo" style={{ width: '200px' }} />
+    </div>  
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">

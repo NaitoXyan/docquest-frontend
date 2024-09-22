28850b63ce73dabab5da8be06ded94d995e56468
Emmanuelle James Duallo <dualloemmanuellejames@gmail.com>
2024-09-22 23:57:27 +0800

    Department Office Pages
    Proj-Lead Notif
    DEPTOFF Dashboard
    DEPTOFF Generate Document
    DEPTOFF Sidebar Page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import LoginPage from "./pages/login/LoginPage";
 import CoordinatorTab from "./pages/coordinator/CoordinatorTab";
 import UserList from "./pages/estaff/UserList";
 import CreateUser from "./pages/estaff/CreateUser";
-import EstaffDocumentsList from "./pages/estaff/EStaffDocumentsList";
+import EStaffDocumentsList from "./pages/estaff/EStaffDocumentsList";
 import EstaffDashboard from "./pages/estaff/EstaffDashboard";
 import EstaffViewDocument from "./pages/estaff/EstaffViewDocuments";
 import ProjLeadDashboard from "./pages/projectlead/ProjLeadDashboard";
+import Topbar from "./components/Topbar";
+import DeptOffSideBar from './components/DeptOffSideBar';
+import DeptOffDashboard from "./pages/deptoff/DeptOffDashboard";
+import DeptOffGenerateDocument from "./pages/deptoff/DeptOffGenerateDocument";
+import DeptOffProfilePage from "./pages/deptoff/DeptOffProfilePage";
 
 function App() {
   return (
     <div className="App">
-      <ProjLeadDashboard />
+      <DeptOffProfilePage/>
     </div>
   );
 }

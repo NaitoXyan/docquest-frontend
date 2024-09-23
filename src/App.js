e79f30892f71d697c54bf262126e0dd7e8d58201
Emmanuelle James Duallo <dualloemmanuellejames@gmail.com>
2024-09-24 00:25:10 +0800

    Project Leader Page
    Proj-Lead Proposal Form

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ import DeptOffSideBar from './components/DeptOffSideBar';
 import DeptOffDashboard from "./pages/deptoff/DeptOffDashboard";
 import DeptOffGenerateDocument from "./pages/deptoff/DeptOffGenerateDocument";
 import DeptOffProfilePage from "./pages/deptoff/DeptOffProfilePage";
+import ProjLeadProposalForm from "./pages/projectlead/ProjLeadProposalForm";
 
 function App() {
   return (
     <div className="App">
-      <DeptOffProfilePage/>
+      <ProjLeadProposalForm/>
     </div>
   );
 }

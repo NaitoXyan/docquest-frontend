b98ced4c14d4a4bda402243cd1a312e967c92dfe
GuylordSS <sagun.guylordsebastian56gmail.com>
2024-11-09 15:19:50 +0800

    aaa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Routers, Routes, Route, Navigate} from  "react-router-dom";
+import { BrowserRouter, Routers, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import CoordinatorTab from "./pages/coordinator/CoordinatorTab";
 import UserList from "./pages/estaff/UserList";
@@ -17,15 +17,18 @@ import CoordinatorDashboard from "./pages/coordinator/CoordinatorDashboard";
 import CoordinatorProfilePage from "./pages/coordinator/ProfilePage";
 import SignatoryDashboard from "./pages/signatory/SignatoryDashboard";
 import ProjLeadProposalForm from "./pages/projectlead/ProjLeadProposalForm";
+import ProjLeadMOAForm from "./pages/projectlead/ProjLeadMOAform";
 import ProjLeadProfilePage from "./pages/projectlead/ProjLeadProfilePage";
 import ProjLeadLoadTrainer from "./pages/projectlead/ProjLeadLoadTrainer";
+import ProjLeadDocList from "./pages/projectlead/ProjLeadDocList";
 import MyDocument from "./components/GeneratePdf";
+import MOAPDF from "./components/GenerateMOA-PDF";
+import MyDocument1 from "./components/GeneratePdf copy";
 import { PDFViewer } from "@react-pdf/renderer";
 import ProposalFormSecondPage from "./components/ProposalFormSecondPage";
 
 function App() {
   return (
-    
     // <Routes>
     //   {/* Login */}
     //   <Route path="/" element={<Navigate to="/login" />} />
@@ -35,6 +38,7 @@ function App() {
     //   <Route path="/create_proposal" element={ <ProjLeadProposalForm /> } />
     //   <Route path="/profile" element={ <ProjLeadProfilePage /> } />
     //   <Route path="/load_trainer" element={ <ProjLeadLoadTrainer /> } />
+    //   <Route path="/doc_list" element={ <ProjLeadDocList /> } />
 
     //   {/* create proposal routes */}
     //   <Route path="/proposal_form_second_page" element={ <ProposalFormSecondPage /> }/>
@@ -44,12 +48,12 @@ function App() {
     //   <Route path="/deptoff" element={ <DeptOffDashboard /> } />
     //   <Route path="/coordinator" element={ <CoordinatorDashboard /> } />
     //   <Route path="/signatory" element={ <SignatoryDashboard /> } />
-    
+
     // </Routes>
 
     <div style={{ height: '100vh' }}>
       <PDFViewer style={{ width: '100%', height: '100%' }}>
-        <MyDocument />
+        <MOAPDF />
       </PDFViewer>
     </div>
   );

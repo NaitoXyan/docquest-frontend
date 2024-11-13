07fc06a1082aa5e2ae169d82a96e4b860e4e568b
Chalbert Reyes <chalbertareyes@gmail.com>
2024-11-13 15:26:51 +0800

    aaah

--- src/pages/director/DirectorReviewProject.js
+++ src/pages/director/DirectorReviewProject.js
@@ -28,23 +28,28 @@ const DirectorReviewProject = () => {
       {/* Approval Panel */}
       <div className="w-1/5 fixed right-0 top-0 h-full p-4 bg-white shadow-lg">
         <h3 className="text-xl font-semibold mb-4">Pending For Approval:</h3>
-        <button className="w-full py-2 mb-2 text-white font-bold bg-green-600 rounded hover:bg-green-700">
-          Approve
-        </button>
-        <button className="w-full py-2 mb-2 text-white font-bold bg-red-600 rounded hover:bg-red-700">
-          Disapprove
-        </button>
-        <div className="mt-4">
-          <h4 className="text-lg font-semibold mb-2">Need Revision</h4>
-          <textarea
+        <label>Comment:</label>
+        <textarea
             placeholder="Comment..."
             className="w-full p-2 border rounded mb-2 resize-none"
             rows="4"
           ></textarea>
+        <button className="w-full py-2 mb-2 text-white font-bold bg-green-600 rounded hover:bg-green-700">
+          Approve
+        </button>
+        <button className="w-full py-2 mb-2 text-white font-bold bg-yellow-500 rounded hover:bg-yellow-700">
+          Revise
+        </button>
+        <button className="w-full py-2 mb-2 text-white font-bold bg-red-600 rounded hover:bg-red-700">
+          Deny
+        </button>
+        {/* <div className="mt-4">
+          <h4 className="text-lg font-semibold mb-2">Need Revision</h4>
+          
           <button className="w-full py-2 text-white font-bold bg-blue-600 rounded hover:bg-blue-700">
             Notify For Revision
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );

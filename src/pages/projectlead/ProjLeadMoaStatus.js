9d86bc5c5860bf777cfcf952c7191f7d32b09854
Chalbert Reyes <125259061+NaitoXyan@users.noreply.github.com>
2024-11-13 22:29:22 +0800

    Update ProjLeadMoaStatus.js

--- src/pages/projectlead/ProjLeadMoaStatus.js
+++ src/pages/projectlead/ProjLeadMoaStatus.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
-
 const ProjLeadMoaStatus = () => {
   const [moas, setMoas] = useState([]);
   const userID = localStorage.getItem('userid');

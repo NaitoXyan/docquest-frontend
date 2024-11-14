c692cfc57da590a69954054df0075e8eb8c72c5b
GuylordSS <sagun.guylordsebastian56@gmail.com>
2024-11-14 16:04:07 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import {BrowserRouter, Routers, Routes, Route, Navigate} from  "react-router-dom";
-import { BrowserRouter, Routers, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { BrowserRouter, Routers, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import CoordinatorTab from "./pages/coordinator/CoordinatorTab";
@@ -36,26 +34,12 @@ import ProjLeadMoaStatus from "./pages/projectlead/ProjLeadMoaStatus";
 import ProjLeadEditProject from "./pages/projectlead/ProjLeadEditProject";
 import ProjLeadEditMoaForm from "./pages/projectlead/ProjLeadEditMoaForm";
 import DirectorReviewProject from "./pages/director/DirectorReviewProject";
-
-import PickProjCreateMoa from "./pages/projectlead/ProjLeadPickProjCreateMoa";
-import ProjLeadMoaStatus from "./pages/projectlead/ProjLeadMoaStatus";
 import ProposalFormFirstPage_Deliverables from "./components/ProposalFormFirstPage_Deliverables";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={ <LoginPage /> } />
-      <Route path="/user" element={ <ProjLeadDashboard /> } />
-      <Route path="/estaff" element={ <EstaffDashboard /> } />
-      <Route path="/deptoff" element={ <DeptOffDashboard /> } />
-      <Route path="/coordinator" element={ <CoordinatorDashboard /> } />
-      <Route path="/signatory" element={ <SignatoryDashboard /> } />
-    </Routes>
-
-        Login
-
         {/* Login */}
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
@@ -65,34 +49,16 @@ function App() {
         <Route path="/create_proposal" element={<ProjLeadProposalForm />} />
         <Route path="/profile" element={<ProjLeadProfilePage />} />
         <Route path="/load_trainer" element={<ProjLeadLoadTrainer />} />
-
         <Route path="/project-status/:statusFilterParam" element={<ProjLeadProjectStatus />} /> "need UI/UX"
         <Route path="/pick-project-create-moa" element={<PickProjCreateMoa/>}/> "need UI/UX"
         <Route path="/create_moa/:projectID" element={<ProjLeadMOAForm />}/>
-        <Route path="/moa-status/:statusFilterParam" element={<ProjLeadMoaStatus/>} />
-        <Route path="/ProposalFormFirstPage_Deliverables" element={<ProposalFormFirstPage_Deliverables/>} />
 
         {/* Create proposal routes */}
         {/* <Route path="/proposal_form_second_page" element={<ProposalFormSecondPage />} /> */}
 
         {/* Other Unorganized */}
         <Route path="/pdf-viewer/:projectID" element={<ProjectPDFViewer />} />
-        <Route path="/edit-project/:projectID" element={<EditProposalForm />} /> "need UI/UX"
-
-        <Route path="/project-status/:statusFilterParam" element={<ProjLeadProjectStatus />} />
-        <Route path="/pick-project-create-moa" element={<PickProjCreateMoa/>}/>
-        <Route path="/create_moa/:projectID" element={<ProjLeadMOAForm />}/>
-        <Route path="/moa-status/:statusFilterParam" element={<ProjLeadMoaStatus/>} />
-        <Route path="/edit-moa/:moaID" element={<ProjLeadEditMoaForm/>}/>
-
-        <Route path="/review-project/:projectID" element={<DirectorReviewProject/>} />
-
-        {/* Create proposal routes */}
-        {/* <Route path="/proposal_form_second_page" element={<ProposalFormSecondPage />} /> */}
-
-        {/* Other Unorganized */}
-        <Route path="/pdf-viewer/:projectID" element={<ProjectPDFViewer />} />
-        <Route path="/edit-project/:projectID" element={<ProjLeadEditProject />} />
+        <Route path="/edit-project/:projectID" element={<EditProposalForm />} />
         <Route path="/estaff" element={<EstaffDashboard />} />
         <Route path="/deptoff" element={<DeptOffDashboard />} />
         <Route path="/coordinator" element={<CoordinatorDashboard />} />
